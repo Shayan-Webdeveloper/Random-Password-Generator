b95bcc29b0899adc04775b5aae64adb0fb05503c
Shayan-Webdeveloper <shayandec2@gmail.com>
2026-03-30 23:05:59 +0500

    Task Done

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
 let passwordBox = document.querySelector("#password")
-let length = 12;
 let btn = document.querySelector("button")
+let copyIcon = document.querySelector(".display img")
+let length = 12;
 let upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 let lowerCase = "abcdefghijklmnopqrstuvwxyz"
 let num = "0123456789";
 let symbol = "!@#$%^&*()-_=+[]{}|:;',<>./?"
 let allChars = upperCase + lowerCase + num + symbol;
-let copyIcon = document.querySelector(".display img")
 btn.addEventListener("click", () => {
      let password = "";
      password += upperCase[Math.floor(Math.random() * upperCase.length)]
      password += lowerCase[Math.floor(Math.random() * lowerCase.length)]
      password += num[Math.floor(Math.random() * num.length)]
      password += symbol[Math.floor(Math.random() * symbol.length)]
-while (password.length < length) {
+for (let i = password.length; i < length; i++) {
     password += allChars[Math.floor(Math.random() * allChars.length)]
 }
+
 passwordBox.value = password;
 })
 copyIcon.addEventListener("click", () => {
